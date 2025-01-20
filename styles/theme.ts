d0d7201ab8a92b1e92b3a4ac1bcea92fe185d69c
Đinh Công Thành <cngthnh22112002@gmail.com>
2025-01-20 21:50:42 +0700

    feat: add theme for button

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,9 +1,25 @@
 'use client';
 import { createTheme } from '@mui/material/styles';
+import { green } from '@mui/material/colors';
 
 const theme = createTheme({
-  typography: {
-    fontFamily: 'var(--font-roboto)',
+  palette: {
+    primary: {
+      main: green[500], 
+    },
+    secondary: {
+      main: green[700], 
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: '8px',
+          textTransform: 'none',
+        },
+      },
+    },
   },
 });
 
